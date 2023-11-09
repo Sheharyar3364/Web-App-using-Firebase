5e4b13cebc56c0a991a3fc4e4af37b54f29b3769
Muhammad Sheharyar <96918155+Sheharyar3364@users.noreply.github.com>
2023-11-09 19:40:24 +0000

    added more files

--- application.py
+++ application.py
@@ -1,10 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, request, flash, redirect, url_for
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
+mail = Mail(app)
 
-@app.route("/")
-def hello():
-    return "hello, world! from sheharyar we will see"
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run(debug=True)    
+@app.route('/submit_form', methods=['POST'])
+def submit_form():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+
+        msg = Message('New Contact Form Submission',
+                      recipients=['dummy@gmail.com'])
+        msg.body = f"Name: {name}\nEmail: {email}\nMessage: {message}"
+
+        try:
+            mail.send(msg)
+            flash('Message sent successfully!', 'success')
+        except Exception as e:
+            flash(f"Error: {str(e)}", 'danger')
+
+        return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
